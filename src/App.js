b88c994a7c525cee553e0e50a55a5e5d87e510f4
Faith Haas <Faith.Haas@coxautoinc.com>
2024-09-23 21:48:16 -0400

    meta tags

--- src/App.js
+++ src/App.js
@@ -2,9 +2,14 @@ import './App.css';
 import Header from './components/Header.js';
 import Main from './components/Main.js';
 import Footer from './components/Footer.js';
+import logo from './images/nav-logo.svg';
 function App() {
   return (
       <>
+          <meta name="description" content="Little Lemon is a family-owned restaurant that prides itself in serving fresh and unique Mediterranean cuisine within the Chicago area."/>
+          <meta name="og:title" content="Little Lemon Restaurant"/>
+          <meta name="og:description" content="Little Lemon is a family-owned restaurant that prides itself in serving fresh and unique Mediterranean cuisine within the Chicago area."/>
+          <meta name="og:image" content={logo} />
           <Header></Header>
           <Main></Main>
           <Footer></Footer>
